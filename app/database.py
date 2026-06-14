ff3e864e221133aed722be18070cc7eba83c5439
AnvitaAnanya <anvitaananya93@gmail.com>
2026-06-14 23:15:45 +0530

    Fix database.py for production

--- app/database.py
+++ app/database.py
@@ -2,24 +2,14 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
-import sys
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-connect_args = {}
-if sys.platform == "win32":
-    import ssl
-    ssl_context = ssl.create_default_context()
-    ssl_context.check_hostname = False
-    ssl_context.verify_mode = ssl.CERT_NONE
-    connect_args = {"ssl": ssl_context}
-
 engine = create_async_engine(
     DATABASE_URL,
     echo=False,
-    connect_args=connect_args
 )
 
 AsyncSessionLocal = sessionmaker(
